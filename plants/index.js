47c5ae5def0b024de750e88600e21da013da25cc
Slava Manzhela <slavamanzh@gmail.com>
2023-02-03 16:20:54 +0300

    feat: add incorrect blur effect and working accordion in prices section

--- plants/index.js
+++ plants/index.js
@@ -1,5 +1,3 @@
-document.querySelector("header").classList.toggle("open");
-
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("burger").addEventListener("click", function(){
         document.querySelector("header").classList.toggle("open")
@@ -18,4 +16,142 @@ document.addEventListener("DOMContentLoaded", function(){
     })
 });
 
-console.log("all necessary task request are met");
+/*focus on services*/
+const gardenButton = document.getElementById('gard-btn');
+
+gardenButton.addEventListener('click', (event) => {
+    document.getElementById('gard-btn').classList.toggle('pushed');
+})
+gardenButton.addEventListener('click', (event) => {
+    document.querySelector('.planting-blur').classList.add('blur');
+})
+gardenButton.addEventListener('click', (event) => {
+    document.querySelector('.lawn-blur').classList.add('blur');
+})
+gardenButton.addEventListener('click', (event) => {
+    document.querySelector('.planting-blur2').classList.add('blur');
+})
+gardenButton.addEventListener('click', (event) => {
+    document.querySelector('.planting-blur3').classList.add('blur');
+})
+gardenButton.addEventListener('click', (event) => {
+    document.querySelector('.garden-blur').classList.remove('blur');
+})
+gardenButton.addEventListener('click', (event) => {
+    document.querySelector('.garden-blur2').classList.remove('blur');
+})
+
+
+const lawnButton = document.getElementById('lawn-btn');
+
+lawnButton.addEventListener('click', (event) => {
+    document.getElementById('lawn-btn').classList.toggle('pushed');
+})
+lawnButton.addEventListener('click', (event) => {
+    document.querySelector('.planting-blur').classList.add('blur');
+})
+lawnButton.addEventListener('click', (event) => {
+    document.querySelector('.planting-blur2').classList.add('blur');
+})
+lawnButton.addEventListener('click', (event) => {
+    document.querySelector('.planting-blur3').classList.add('blur');
+})
+lawnButton.addEventListener('click', (event) => {
+    document.querySelector('.garden-blur').classList.add('blur');
+})
+lawnButton.addEventListener('click', (event) => {
+    document.querySelector('.garden-blur2').classList.add('blur');
+})
+lawnButton.addEventListener('click', (event) => {
+    document.querySelector('.lawn-blur').classList.remove('blur');
+})
+
+/*Blur on service section------------------------*/
+
+const plantingButton = document.getElementById('planting-btn');
+
+plantingButton.addEventListener('click', (event) => {
+    document.getElementById('planting-btn').classList.toggle('pushed');
+})
+plantingButton.addEventListener('click', (event) => {
+    document.querySelector('.planting-blur').classList.remove('blur');
+})
+plantingButton.addEventListener('click', (event) => {
+    document.querySelector('.planting-blur2').classList.remove('blur');
+})
+plantingButton.addEventListener('click', (event) => {
+    document.querySelector('.planting-blur3').classList.remove('blur');
+})
+plantingButton.addEventListener('click', (event) => {
+    document.querySelector('.garden-blur').classList.add('blur');
+})
+plantingButton.addEventListener('click', (event) => {
+    document.querySelector('.garden-blur2').classList.add('blur');
+})
+plantingButton.addEventListener('click', (event) => {
+    document.querySelector('.lawn-blur').classList.add('blur');
+})
+
+/*Price section accordion-------------*/
+/*Basics*/
+document.getElementById('arr-prices-1').addEventListener('click', (event)=>{
+    document.querySelector('.prcs-panels:nth-child(2)')
+    .classList.remove('hide');
+})
+
+document.getElementById('arr-prices-1').addEventListener('click', (event)=>{
+    document.querySelector('.price-buttons div.price-btn:nth-child(1)')
+    .classList.add('on-click')
+})
+
+document.querySelector('.prcs-panels:nth-child(2) img').addEventListener('click', (event) => {
+    document.querySelector('.prcs-panels:nth-child(2)')
+    .classList.add('hide');
+})
+
+document.querySelector('.prcs-panels:nth-child(2) img').addEventListener('click', (event) => {
+    document.querySelector('.price-buttons div.price-btn:nth-child(1)')
+    .classList.remove('on-click')
+})
+
+/*Standard*/
+document.getElementById('arr-prices-2').addEventListener('click', (event)=>{
+    document.querySelector('.prcs-panels:nth-child(4)')
+    .classList.remove('hide');
+})
+
+document.getElementById('arr-prices-2').addEventListener('click', (event)=>{
+    document.querySelector('.price-buttons div.price-btn:nth-child(3)')
+    .classList.add('on-click')
+})
+
+document.querySelector('.prcs-panels:nth-child(4) img').addEventListener('click', (event) => {
+    document.querySelector('.prcs-panels:nth-child(4)')
+    .classList.add('hide');
+})
+
+document.querySelector('.prcs-panels:nth-child(4) img').addEventListener('click', (event) => {
+    document.querySelector('.price-buttons div.price-btn:nth-child(3)')
+    .classList.remove('on-click')
+})
+
+/*Pro care*/
+document.getElementById('arr-prices-3').addEventListener('click', (event)=>{
+    document.querySelector('.prcs-panels:nth-child(6)')
+    .classList.remove('hide');
+})
+
+document.getElementById('arr-prices-3').addEventListener('click', (event)=>{
+    document.querySelector('.price-buttons div.price-btn:nth-child(5)')
+    .classList.add('on-click')
+})
+
+document.querySelector('.prcs-panels:nth-child(6) img').addEventListener('click', (event) => {
+    document.querySelector('.prcs-panels:nth-child(6)')
+    .classList.add('hide');
+})
+
+document.querySelector('.prcs-panels:nth-child(6) img').addEventListener('click', (event) => {
+    document.querySelector('.price-buttons div.price-btn:nth-child(5)')
+    .classList.remove('on-click')
+})
