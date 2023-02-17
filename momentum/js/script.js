6275e4c29e09dba7b192489a31b513230e99cfe6
Slava Manzhela <slavamanzh@gmail.com>
2023-02-17 23:43:36 +0300

    feat: add raw code of greeting

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,7 +1,9 @@
 const time = document.querySelector('.time');
+const greeting = document.querySelector('.greeting');
 const day = document.querySelector('.date');
 const date = new Date();
 const options = {weekday: 'long', month: 'long', day: 'numeric'};
+const hours = date.getHours();
 
 
 function showTime(date) {
@@ -13,5 +15,16 @@ function showTime(date) {
     
     setTimeout(showTime, 1000);
 }
-
 showTime();
+
+function getTimeOfDay(hours) {
+    if ((hours>=0)&&(hours<6)){
+        return "night";
+    } else if ((hours>=6)&&(hours<12)){
+        return "morning";
+    } else if ((hours>=12)&&(hours<18)){
+        return "afternoon";
+    } else {
+       return "evening";
+    }
+}
