91fea7486547b9b2a74eae64dcd426a59d510625
Slava Manzhela <slavamanzh@gmail.com>
2023-02-25 17:18:00 +0300

    feat: complete weather gadget

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -56,7 +56,7 @@ function getLocalStorage() {
 }
 window.addEventListener('load', getLocalStorage);
 
-
+/*-----Background slider-----*/
 function getRandomNum() {
     return Math.floor(Math.random()*20 + 1);
 }
@@ -97,3 +97,43 @@ function getNextSlide () {
 nextSlide.addEventListener("click", function(){
     getNextSlide();
 })
+
+/*----------Weather----------*/
+let city = document.querySelector('.city');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const windSpeed = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+
+function setLocalStorageCity() {
+    localStorage.setItem('city', city.value);
+}
+window.addEventListener('beforeunload', setLocalStorageCity);
+window.addEventListener('keydown', setLocalStorageCity);
+
+function getLocalStorageCity() {
+    if (localStorage.getItem('city')) {
+        city.value = localStorage.getItem('city');
+    } else {
+        city.value = 'Minsk';
+    } 
+}
+window.addEventListener('load', getLocalStorageCity);
+
+let cityName = localStorage.getItem('city');
+
+async function getWeather() {  
+    let url = new URL(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&lang=en&appid=b80f7136e278c0a485ad4b77b3130b55&units=metric`);
+    const res = await fetch(url);
+    const data = await res.json(); 
+    
+    city.textContent = data.name;
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${Math.round(data.main.temp)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+    windSpeed.textContent = `Wind speed: ${Math.round(data.wind.speed)}m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+}
+getWeather();
+
