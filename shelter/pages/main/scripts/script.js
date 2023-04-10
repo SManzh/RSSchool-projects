bed964772e7d6ef036376c509a6a487c3795bb72
Slava Manzhela <slavamanzh@gmail.com>
2023-04-10 23:56:54 +0300

    feat: add draft of carousel

--- shelter/pages/main/scripts/script.js
+++ shelter/pages/main/scripts/script.js
@@ -3,3 +3,22 @@ const burger = document.querySelector(".header__burger-btn");
 burger.addEventListener("click", () => {
   document.querySelector(".header").classList.toggle("open")
 })
+
+/*-----Slider-----*/
+const sliderWidth = document.querySelector(".slider__cards").clientWidth;
+const sliderCardWidth = document.querySelector(".slider__card").clientWidth + 1;
+const maxCards = Math.floor(sliderWidth/sliderCardWidth);
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+const CAROUSEL = document.querySelector(".slider__cards");
+
+btnLeft.addEventListener("click", () => {
+  CAROUSEL.classList.add("transition-left");
+  CAROUSEL.classList.remove("transition-right");
+})
+
+btnRight.addEventListener("click", () => {
+  CAROUSEL.classList.add("transition-right");
+  CAROUSEL.classList.remove("transition-left");
+})
