cd01094b83e9f9b83c253343b0d5e20e999e84e6
Slava Manzhela <slavamanzh@gmail.com>
2023-04-25 16:59:53 +0300

    feat: minor changes to the project

--- shelter/pages/pets/scripts/script-pet.js
+++ shelter/pages/pets/scripts/script-pet.js
@@ -3,3 +3,8 @@ const burger = document.querySelector(".header__burger-btn");
 burger.addEventListener("click", () => {
   document.querySelector(".header").classList.toggle("open")
 })
+
+document.querySelectorAll(".content__card").addEventListener("click", () => {
+  document.querySelector(".pets__pop-up").classList.add("active");
+})
+document.querySelector(".pets__pop-up")
